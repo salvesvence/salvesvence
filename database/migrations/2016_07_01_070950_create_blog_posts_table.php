e019b46b87cb81ec93e15c77f8fbb6c1de48f5b7
salvesvence <salvesvence@gmail.com>
2016-07-01 09:12:32 +0200

    Craeted the create_blog_posts_table migration and its models.

---
+++ database/migrations/2016_07_01_070950_create_blog_posts_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('author_id');
+            $table->unsignedInteger('category_id')->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::create('blog_posts_translations', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('blog_post_id');
+            $table->string('locale')->index();
+
+            $table->string('slug');
+            $table->string('title');
+            $table->text('intro')->nullable();
+            $table->text('body')->nullable();
+        });
+
+        Schema::table('blog_posts', function (Blueprint $table) {
+            $table->foreign('author_id')
+                ->references('id')->on('blog_authors');
+
+            $table->foreign('category_id')
+                ->references('id')->on('blog_categories');
+        });
+
+        Schema::table('blog_posts_translations', function (Blueprint $table) {
+            $table->foreign('blog_post_id')
+                ->references('id')->on('blog_post')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts_translations');
+        Schema::dropIfExists('blog_posts');
+    }
+}
