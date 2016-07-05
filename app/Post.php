14102b4f43da22c6ff0104f179db1089e79c9366
salvesvence <salvesvence@gmail.com>
2016-07-05 08:29:25 +0200

    Created the author(), category() and tags() function into the Post model class.

--- app/Post.php
+++ app/Post.php
@@ -1,10 +1,41 @@
-<?php
+<?php namespace App;
 
-namespace App;
-
+use Dimsav\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use Translatable;
+
+    public $translatedAttributes = ['slug', 'title', 'intro', 'body'];
+
+    /**
+     * User associated with the current post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'author_id');
+    }
+
+    /**
+     * Category associated with the current post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    /**
+     * Tags associated with the current post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 }
