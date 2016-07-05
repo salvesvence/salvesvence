79922b344294cfa76917993e4dc4da987ecbe3b1
salvesvence <salvesvence@gmail.com>
2016-07-05 09:32:02 +0200

    Added a custom value into the  variable of CategoryTranslation model class.

--- app/CategoryTranslation.php
+++ app/CategoryTranslation.php
@@ -4,5 +4,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryTranslation extends Model
 {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'categories_translations';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
     public $timestamps = false;
 }
