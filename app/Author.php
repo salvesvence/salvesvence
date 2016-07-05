b20a034c16150fffe9cd9e7ff134b5d0c98c815c
salvesvence <salvesvence@gmail.com>
2016-07-05 08:07:07 +0200

    Created the posts() relationship function into the Author model class.

--- app/Author.php
+++ app/Author.php
@@ -1,10 +1,21 @@
-<?php
+<?php namespace App;
 
-namespace App;
-
+use Dimsav\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    use Translatable;
+
+    public $translatedAttributes = ['slug', 'firstname', 'lastname', 'info'];
+
+    /**
+     * Posts associated with the current author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
