fa0588753b56696b67f8502c010daa6897a95204
salvesvence <salvesvence@gmail.com>
2016-07-06 11:59:40 +0200

    Created the category model factory.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'slug' => function (array $category) {
+            return str_slug($category['name']);
+        }
+    ];
+});
