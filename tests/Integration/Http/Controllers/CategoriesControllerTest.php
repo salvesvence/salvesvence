5fb5f9fd2772c16716e61f873fddc0de4182830e
salvesvence <salvesvence@gmail.com>
2016-07-06 12:05:04 +0200

    Created the it_see_all_cateories_by_current_locale() test into the CategoriesControllerTest class.

--- tests/Integration/Http/Controllers/CategoriesControllerTest.php
+++ tests/Integration/Http/Controllers/CategoriesControllerTest.php
@@ -1,16 +1,41 @@
 <?php namespace Integration\Http\Controllers;
 
+use App\Category;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use TestCase;
 
 class CategoriesControllerTest extends TestCase {
 
     use WithoutMiddleware;
+    use DatabaseTransactions;
 
     /** @test */
-    function it_see_all_cateories()
+    function it_see_all_cateories_by_current_locale()
     {
+        $stCategory = new Category;
+
+        $stCategory->translateOrNew('en')->name = "Web Development";
+        $stCategory->translateOrNew('en')->slug = "web-development";
+
+        $stCategory->translateOrNew('es')->name = "Desarrollo Web";
+        $stCategory->translateOrNew('es')->slug = "desarrollo-web";
+
+        $stCategory->save();
+
+        app()->setLocale('es');
+
         $this->visit('/categories')
-             ->see('Categories');
+             ->see('desarrollo-web');
+
+        app()->setLocale('en');
+
+        $this->visit('/categories')
+             ->see('web-development');
+
+        app()->setLocale('fr');
+
+        $this->visit('/categories')
+             ->see('');
     }
 }
