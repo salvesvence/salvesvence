84f85e4f256d3220d312aab28c6e65fc94621711
salvesvence <salvesvence@gmail.com>
2016-07-06 08:29:35 +0200

    Created the it_see_all_categories() test into the CategoriesControllerTest class with the initial content.

--- tests/Integration/Http/Controllers/CategoriesControllerTest.php
+++ tests/Integration/Http/Controllers/CategoriesControllerTest.php
@@ -1,7 +1,16 @@
 <?php namespace Integration\Http\Controllers;
 
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use TestCase;
 
 class CategoriesControllerTest extends TestCase {
 
+    use WithoutMiddleware;
+
+    /** @test */
+    function it_see_all_cateories()
+    {
+        $this->visit('/categories')
+             ->see('Categories');
+    }
 }
