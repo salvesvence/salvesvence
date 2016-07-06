0a844ebff6817cc68d700a53e8c721902f7b7cfb
salvesvence <salvesvence@gmail.com>
2016-07-06 12:57:11 +0200

    Added the initial form into the categories.create view.

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -5,9 +5,37 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Crear Categoría:</div>
 
-                <h1>Crear Categoría:</h1>
+                    <div class="panel-body">
 
+                        <!-- #store-category form -->
+
+                        <form id="store-category" action="{{ route('categories.store') }}" method="post">
+
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                            <!-- name form input -->
+
+                            <div class="form-group col-sm-12">
+
+                                <label for="name">Nombre</label>
+
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Nombre">
+
+                            </div>
+
+                            <div class="form-group col-sm-12">
+
+                                <input type="submit" class="btn" value="GUARDAR"/>
+
+                            </div>
+
+                        </form>
+
+                    </div>
+                </div>
             </div>
         </div>
     </div>
