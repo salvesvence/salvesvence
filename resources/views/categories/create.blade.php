a1b5a0171ccb2817d70eccc42ee7fa1e33c495f6
salvesvence <salvesvence@gmail.com>
2016-07-13 10:39:01 +0200

    Created the back button into the categories/create view.

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -9,31 +9,26 @@
                     <div class="panel-heading">Crear Categoría:</div>
 
                     <div class="panel-body">
-
-                        <!-- #store-category form -->
-
                         <form id="store-category" action="{{ route('categories.store') }}" method="post">
 
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                            <!-- name form input -->
-
                             <div class="form-group col-sm-12">
-
                                 <label for="name">Nombre</label>
-
                                 <input type="text" class="form-control" id="name" name="name" placeholder="Nombre">
-
                             </div>
 
-                            <div class="form-group col-sm-12">
-
+                            <div class="form-group col-xs-6">
                                 <input type="submit" class="btn" name="save" value="GUARDAR">
+                            </div>
 
+                            <div class="form-group col-xs-6">
+                                <a href="{{ URL::previous() }}" class="btn btn-primary pull-right">
+                                    <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span> ATRÁS
+                                </a>
                             </div>
 
                         </form>
-
                     </div>
 
                 </div>
