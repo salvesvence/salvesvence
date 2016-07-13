7c8413bd1aac57ff09006d97203ac90b54778c6e
salvesvence <salvesvence@gmail.com>
2016-07-13 12:24:25 +0200

    Created the tags/index view.

---
+++ resources/views/tags/index.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Tags:</div>
+
+                    <div class="panel-body">
+
+                        <table class="table table-striped table-bordered">
+
+                            <thead>
+
+                            <tr>
+
+                                <td>#</td>
+                                <td>Nombre</td>
+                                <td class="text-center">Editar</td>
+                                <td class="text-center">Borrar</td>
+
+                            </tr>
+
+                            </thead>
+
+                            <tbody>
+
+                            @foreach($tags as $tag)
+
+                                <tr>
+
+                                    <td>{{ $tag->id }}</td>
+                                    <td>{{ $tag->name }}</td>
+                                    <td>
+                                        <a href="{{ route('tags.edit', $tag->slug) }}" class="btn-block text-center">
+                                            <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('tags.destroy', $tag->slug) }}" class="btn-block text-center">
+                                            <span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
+                                        </a>
+                                    </td>
+
+                                </tr>
+
+                            @endforeach
+
+                            </tbody>
+
+                        </table>
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <a href="{{ route('tags.create') }}">
+                                    <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> EDITAR
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
