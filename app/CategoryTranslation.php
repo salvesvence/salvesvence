af03ca7364055556a595a862d5b3a6797d085a51
salvesvence <salvesvence@gmail.com>
2016-07-13 11:27:38 +0200

    Added the fillable variables into the CategoriesTranslation model class.

--- app/CategoryTranslation.php
+++ app/CategoryTranslation.php
@@ -12,6 +12,13 @@ class CategoryTranslation extends Model
     protected $table = 'categories_translations';
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['slug', 'name'];
+
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
