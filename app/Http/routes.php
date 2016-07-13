73b80b6e52f2050127505e60eac37ad9299f5aa2
salvesvence <salvesvence@gmail.com>
2016-07-13 11:44:21 +0200

    Created the categories.destroy route.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,6 +40,48 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
-Route::resource('categories', 'CategoriesController');
+
+/*
+|--------------------------------------------------------------------------
+| Categories Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+Route::resource('categories', 'CategoriesController', ['except' =>
+    ['destroy']
+]);
+
+Route::get('categories/{slug}/delete', [
+    'as' => 'categories.destroy',
+    'uses' => 'CategoriesController@destroy'
+]);
+
+
+/*
+|--------------------------------------------------------------------------
+| Tags Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
 Route::resource('tags', 'TagsController');
+
+
+/*
+|--------------------------------------------------------------------------
+| Posts Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
 Route::resource('posts', 'PostsController');
