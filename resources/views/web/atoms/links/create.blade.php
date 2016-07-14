71c1bf89757750ed7ddfdc605f8a8462dfac5c2b
salvesvence <salvesvence@gmail.com>
2016-07-14 10:35:33 +0200

    Fixed the name of atoms/links/create view.

--- resources/views/web/atoms/links/create.blade.php
+++ resources/views/web/atoms/links/create.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ isset($route) ? $route : '#' }}">
-    <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> EDITAR
+    <span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> CREAR
 </a>
