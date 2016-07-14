11f2f7f2a78cb3849625d6e68e2638819da05182
salvesvence <salvesvence@gmail.com>
2016-07-14 10:06:38 +0200

    Created the it_see_all_tags_by_current_locale() test function and the createTag() function into the TagsControllerTest class.

--- tests/Integration/Http/Controllers/TagsControllerTest.php
+++ tests/Integration/Http/Controllers/TagsControllerTest.php
@@ -1,5 +1,6 @@
 <?php namespace Integration\Http\Controllers;
 
+use App\Tag;
 use TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -9,4 +10,39 @@ class TagsControllerTest extends TestCase {
     use WithoutMiddleware;
     use DatabaseTransactions;
 
+    /** @test */
+    function it_see_all_tags_by_current_locale()
+    {
+        $this->createTag();
+
+        app()->setLocale('es');
+
+        $this->visit('/tags')
+            ->see('desarrollo-web');
+
+        app()->setLocale('en');
+
+        $this->visit('/tags')
+            ->see('web-development');
+
+        app()->setLocale('fr');
+
+        $this->visit('/tags')
+            ->see('');
+    }
+
+    private function createTag()
+    {
+        $tag = new Tag;
+
+        $tag->translateOrNew('en')->name = "Web Development";
+        $tag->translateOrNew('en')->slug = "web-development";
+
+        $tag->translateOrNew('es')->name = "Desarrollo Web";
+        $tag->translateOrNew('es')->slug = "desarrollo-web";
+
+        $tag->save();
+
+        return $tag;
+    }
 }
