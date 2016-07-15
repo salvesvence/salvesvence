2be703575bcc40f691e040316520c088e1a0f754
salvesvence <salvesvence@gmail.com>
2016-07-15 13:33:24 +0200

    Created the PostPolicy class.

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Post;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PostPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine if the given post can be updated by the user.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Post  $post
+     * @return bool
+     */
+    public function update(User $user, Post $post)
+    {
+        return $user->hasRole('admin') || $user->id === $post->user_id;
+    }
+}
