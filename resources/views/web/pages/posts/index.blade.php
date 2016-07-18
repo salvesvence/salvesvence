f280ae3287402f420d14d44cafbd8615c53f10b9
salvesvence <salvesvence@gmail.com>
2016-07-18 10:06:34 +0200

    Created the posts.index view.

---
+++ resources/views/web/pages/posts/index.blade.php
@@ -0,0 +1,32 @@
+@extends('web.templates.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Categorías:</div>
+
+                    <div class="panel-body">
+
+                        <table class="table table-striped table-bordered">
+                            @include('web.pages.categories.partials.thead')
+                            @include('web.pages.categories.partials.tbody')
+                        </table>
+
+                        <div class="row">
+                            <div class="col-sm-12">
+                                @include('web.atoms.links.create', ['route' => route('posts.create')])
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
