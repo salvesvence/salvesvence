a6f4a7e4f6a9824795c322a6d181797987588570
salvesvence <salvesvence@gmail.com>
2016-07-18 10:55:18 +0200

    Included the thead and tbody elements into the posts/index view.

--- resources/views/web/pages/posts/index.blade.php
+++ resources/views/web/pages/posts/index.blade.php
@@ -11,8 +11,8 @@
                     <div class="panel-body">
 
                         <table class="table table-striped table-bordered">
-                            @include('web.pages.categories.partials.thead')
-                            @include('web.pages.categories.partials.tbody')
+                            @include('web.pages.posts.partials.thead')
+                            @include('web.pages.posts.partials.tbody')
                         </table>
 
                         <div class="row">
