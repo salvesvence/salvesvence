b0fb004e71ac0dda798924fd68ab898265e12d3e
salvesvence <salvesvence@gmail.com>
2016-07-28 13:12:16 +0200

    Added white spaces into the gulpfile config.

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,15 @@ elixir(function(mix) {
     var bootstrapPath = 'node_modules/bootstrap-sass/assets';
 
     mix.sass('app.scss')
+
         .copy(bootstrapPath + '/fonts', 'public/fonts')
         .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'resources/assets/js/bootstrap')
         .copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js/jquery')
+
         .scripts([
             'jquery/jquery.min.js',
             'bootstrap/bootstrap.min.js'
         ], 'public/js/app.js')
+
         .version(['css/app.css', 'js/app.js']);
 });
