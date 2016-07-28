26f61868b88a68506d33c897ef1ebd2f96e656c3
salvesvence <salvesvence@gmail.com>
2016-07-28 13:44:58 +0200

    Added the  variable into the categories.partials.form view.

--- resources/views/web/pages/categories/partials/form.blade.php
+++ resources/views/web/pages/categories/partials/form.blade.php
@@ -1,6 +1,7 @@
 <form id="store-category" action="{{ route('categories.store') }}" method="post">
 
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <input type="hidden" name="locale" value="{{ $locale }}">
 
     <div class="form-group col-sm-12">
         @include('web.atoms.inputs.name')
--- resources/views/web/pages/categories/partials/nav-tab.blade.php
+++ resources/views/web/pages/categories/partials/nav-tab.blade.php
@@ -1,32 +1,29 @@
 <!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
     <li role="presentation" class="active">
-        <a href="#spanish" aria-controls="spanish" role="tab" data-toggle="tab">Español</a>
+        <a href="#es" aria-controls="es" role="tab" data-toggle="tab">Español</a>
     </li>
     <li role="presentation">
-        <a href="#english" aria-controls="english" role="tab" data-toggle="tab">Inglés</a>
+        <a href="#en" aria-controls="en" role="tab" data-toggle="tab">Inglés</a>
     </li>
 </ul>
 
 <!-- Tab panes -->
 <div class="tab-content">
-    <div role="tabpanel" class="tab-pane fade in active" id="spanish">
+    <div role="tabpanel" class="tab-pane fade in active" id="es">
         <div class="row">
             <div class="col-sm-12">
                 <hr>
             </div>
-
-            @include('web.pages.categories.partials.form')
+            @include('web.pages.categories.partials.form', ['locale' => 'es'])
         </div>
     </div>
-
-    <div role="tabpanel" class="tab-pane fade" id="english">
+    <div role="tabpanel" class="tab-pane fade" id="en">
         <div class="row">
             <div class="col-sm-12">
                 <hr>
             </div>
-
-            @include('web.pages.categories.partials.form')
+            @include('web.pages.categories.partials.form', ['locale' => 'en'])
         </div>
     </div>
 </div>
