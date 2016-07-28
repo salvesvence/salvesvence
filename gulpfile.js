9103afcb5609e42c1c0713c8fc685a00e54c15e9
salvesvence <salvesvence@gmail.com>
2016-07-28 13:09:05 +0200

    Added the jquery library as a project dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,12 @@ elixir(function(mix) {
     var bootstrapPath = 'node_modules/bootstrap-sass/assets';
 
     mix.sass('app.scss')
-        .scripts([
-            'bootstrap/bootstrap.min.js'
-        ])
-        .version(['css/app.css', 'js/all.js'])
         .copy(bootstrapPath + '/fonts', 'public/fonts')
-        .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'resources/assets/js/bootstrap');
+        .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'resources/assets/js/bootstrap')
+        .copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js/jquery')
+        .scripts([
+            'jquery/jquery.min.js',
+            'bootstrap/bootstrap.min.js'
+        ], 'public/js/app.js')
+        .version(['css/app.css', 'js/app.js']);
 });
