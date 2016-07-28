f9e3bb6df00eb47d18ef1448d6fd7c6b2c0044a1
salvesvence <salvesvence@gmail.com>
2016-07-28 11:28:03 +0200

    Added all bootstrap necessary files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+elixir.config.sourcemaps = false;
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+
+    var bootstrapPath = 'node_modules/bootstrap-sass/assets';
+
+    mix.sass('app.scss')
+        .version(['css/app.css'])
+        .copy(bootstrapPath + '/fonts', 'public/fonts')
+        .copy(bootstrapPath + '/javascripts/*.js', 'resources/assets/js')
+        .copy(bootstrapPath + '/stylesheets', 'resources/assets/sass');
 });
