9183eb6cc0a1f3a78baa229b35158e4df263c6b2
salvesvence <salvesvence@gmail.com>
2016-07-28 13:33:26 +0200

    Created the categories.partials.nav-tab view.

--- resources/views/web/pages/categories/create.blade.php
+++ resources/views/web/pages/categories/create.blade.php
@@ -9,38 +9,8 @@
                     <div class="panel-heading">Crear Categoría:</div>
 
                     <div class="panel-body">
-
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#spanish" aria-controls="spanish" role="tab" data-toggle="tab">Español</a></li>
-                            <li role="presentation"><a href="#english" aria-controls="english" role="tab" data-toggle="tab">Inglés</a></li>
-                        </ul>
-
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <div role="tabpanel" class="tab-pane fade in active" id="spanish">
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <hr>
-                                    </div>
-
-                                    @include('web.pages.categories.partials.form')
-                                </div>
-                            </div>
-
-                            <div role="tabpanel" class="tab-pane fade" id="english">
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <hr>
-                                    </div>
-
-                                    @include('web.pages.categories.partials.form')
-                                </div>
-                            </div>
-                        </div>
-
+                        @include('web.pages.categories.partials.nav-tab')
                     </div>
-
                 </div>
             </div>
         </div>
