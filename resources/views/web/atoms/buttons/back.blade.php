b47114a412ff761e16e23eeba175d514715d534c
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-01 13:30:43 +0200

    Fixed the back and save buttons partials.

--- resources/views/web/atoms/buttons/back.blade.php
+++ resources/views/web/atoms/buttons/back.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ isset($route) ? $route : URL::previous() }}" class="btn btn-primary pull-right">
+<a href="{{ isset($route) ? $route : URL::previous() }}" class="btn btn-primary">
     <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span> ATRÁS
 </a>
