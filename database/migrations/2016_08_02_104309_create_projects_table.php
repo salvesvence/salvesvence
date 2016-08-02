49034bce881dde4ba78c5d6e6b72863981bbd74d
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-02 12:49:21 +0200

    Created the create_projects_table migration.

---
+++ database/migrations/2016_08_02_104309_create_projects_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+
+        Schema::create('projects_translations', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('project_id');
+            $table->string('locale')->index();
+
+            $table->string('slug');
+            $table->string('name');
+            $table->string('description', 300);
+        });
+
+        Schema::table('categories_translations', function (Blueprint $table) {
+            $table->foreign('project_id')
+                ->references('id')->on('projects')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects_translations');
+        Schema::dropIfExists('projects');
+    }
+}
