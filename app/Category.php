b24c8ccfa7333aaa18a5d4f427d8f3af6af7c0c3
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-03 10:05:17 +0200

    Added the fillable fields into the Category model class.

--- app/Category.php
+++ app/Category.php
@@ -5,6 +5,13 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'slug'];
+
+    /**
      * Posts associated with the current category.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
