ce68f6a7e656ac6ba17ffd436a0737f141d6189f
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-03 07:45:28 +0200

    Deleted white spaces into the categories.index view.

--- resources/views/web/pages/categories/index.blade.php
+++ resources/views/web/pages/categories/index.blade.php
@@ -7,7 +7,6 @@
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-heading">Categorías:</div>
-
                     <div class="panel-body">
 
                         <table class="table table-striped table-bordered">
@@ -22,9 +21,7 @@
                         </div>
 
                     </div>
-
                 </div>
-
             </div>
         </div>
     </div>
