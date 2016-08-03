b6cb4354758cfe96f10c4da30099caa8be980478
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-03 09:03:48 +0200

    Added the fillable fields into the Category model class.

--- app/Tag.php
+++ app/Tag.php
@@ -5,6 +5,13 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'slug'];
+
+    /**
      * Posts associated with the current tag.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
