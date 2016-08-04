4a22322dc7da933a85a22c8c5f06f2e225c2fcda
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-04 10:39:16 +0200

    Fixed the title of the projects/index view.

--- resources/views/web/pages/projects/index.blade.php
+++ resources/views/web/pages/projects/index.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Categorías:</div>
+                    <div class="panel-heading">Proyectos:</div>
 
                     <div class="panel-body">
 
@@ -24,7 +24,6 @@
                     </div>
 
                 </div>
-
             </div>
         </div>
     </div>
