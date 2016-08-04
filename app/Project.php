94761082caddc702a182e43ebf038321ff005d6e
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-04 10:30:28 +0200

    Fixed the fillable fields into the Project model class.

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['slug', 'title', 'description'];
 }
