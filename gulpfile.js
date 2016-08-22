820bfd7515360fb5af3d09300ba8c9b522fe3711
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-22 09:27:10 +0200

    Added the vue and vue-resource files into the scripts task of gulp configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,15 @@ elixir(function(mix) {
         .copy(bootstrapPath + '/fonts', 'public/fonts')
         .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'resources/assets/js/bootstrap')
         .copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js/jquery')
+        .copy([
+            'node_modules/vue/dist/vue.min.js',
+            'node_modules/vue-resource/dist/vue-resource.min.js'
+        ], 'resources/assets/js/vue')
 
         .scripts([
             'jquery/jquery.min.js',
+            'vue/vue.min.js',
+            'vue/vue-resource.min.js',
             'bootstrap/bootstrap.min.js'
         ], 'public/js/app.js')
 
