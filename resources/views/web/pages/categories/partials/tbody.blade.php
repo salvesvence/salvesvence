c536ffb0fe103c35340ea97ffac48db5aad0b54d
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-23 07:59:46 +0200

    Changed the sintax of the categories tbody element.

--- resources/views/web/pages/categories/partials/tbody.blade.php
+++ resources/views/web/pages/categories/partials/tbody.blade.php
@@ -1,20 +1,19 @@
-<tbody>
+<tbody id="categories">
 
-    @foreach($categories as $category)
+    <tr v-for="category in categories">
 
-        <tr>
-
-            <td>{{ $category->id }}</td>
-            <td>{{ $category->name }}</td>
-            <td>
-                @include('web.atoms.links.edit', ['route' => route('categories.edit', $category->slug)])
-            </td>
-            <td>
-                @include('web.atoms.links.delete', ['route' => route('categories.destroy', $category->slug)])
-            </td>
-
-        </tr>
-
-    @endforeach
+        <td>@{{ category.id }}</td>
+        <td>@{{ category.name }}</td>
+        <td>
+            <a href="categories/@{{ category.slug }}/edit" class="btn-block text-center">
+                <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+            </a>
+        </td>
+        <td>
+            <a href="categories/@{{ category.slug }}/delete" class="btn-block text-center">
+                <span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
+            </a>
+        </td>
+    </tr>
 
 </tbody>
