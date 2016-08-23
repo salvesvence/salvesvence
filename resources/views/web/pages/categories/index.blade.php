702d3e5a13316ed9a59ee7ea84f7b51f6ed1f06b
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-23 13:50:18 +0200

    Deleted the useless code lines.

--- resources/views/web/pages/categories/index.blade.php
+++ resources/views/web/pages/categories/index.blade.php
@@ -8,16 +8,37 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">Categorías:</div>
                     <div class="panel-body" id="categories">
-
                         <categories list="{{ json_encode($categories) }}"></categories>
 
                         <template id="categories-template">
-
                             <table class="table table-striped table-bordered">
-                                @include('web.pages.categories.partials.thead')
-                                @include('web.pages.categories.partials.tbody')
-                            </table>
+                                <thead>
+                                    <tr>
+                                        <td>#</td>
+                                        <td>Nombre</td>
+                                        <td class="text-center">Editar</td>
+                                        <td class="text-center">Borrar</td>
+                                    </tr>
+                                </thead>
 
+                                <tbody>
+                                    <tr v-for="category in list">
+                                        <td>@{{ category.id }}</td>
+                                        <td>@{{ category.name }}</td>
+                                        <td>
+                                            <a href="categories/@{{ category.slug }}/edit" class="btn-block text-center">
+                                                <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <a href="categories/@{{ category.slug }}/delete" class="btn-block text-center">
+                                                <span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+
+                            </table>
                         </template>
 
                         <div class="row">
