9c04df0c11fb428d48883972cbcd0d8540f19402
Silvano Alves Vence <salvesvence@gmail.com>
2016-08-31 11:22:19 +0200

    Fixed the grid configuration of the forms footer elements.

--- resources/views/web/molecules/forms/footer.blade.php
+++ resources/views/web/molecules/forms/footer.blade.php
@@ -1,7 +1,7 @@
-<div class="form-group col-sm-6">
+<div class="form-group col-xs-6">
     @include('web.atoms.buttons.back')
 </div>
 
-<div class="form-group col-sm-6">
+<div class="form-group col-xs-6">
     @include('web.atoms.buttons.save')
 </div>
