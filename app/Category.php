51efb12f9e5a78adb672ab35ca189d2fa97e1322
salvesvence <salvesvence@gmail.com>
2016-11-17 22:52:17 +0100

    Added the neccessary code into the Category model class.

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'slug'];
+
+    /**
+     * Create the slug field when we save a new project register.
+     */
+    public static function boot()
+    {
+        parent::boot();
+        static::saving(function($category) {
+            $category->slug = str_slug($category->name);
+        });
+    }
+
+    /**
+     * Posts associated with the current category.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
