9c7c4892c8dc58cff7e29c9b8bfe4fc6eb31101a
salvesvence <salvesvence@gmail.com>
2016-11-17 22:47:10 +0100

    Created the Category model and migration.

--- database/migrations/2016_11_17_214531_create_categories_table.php
+++ database/migrations/2016_11_17_214531_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-
-            $table->string('username')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('firstname')->nullable();
-            $table->string('lastname')->nullable();
-
-            $table->rememberToken();
+            $table->string('slug');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }
