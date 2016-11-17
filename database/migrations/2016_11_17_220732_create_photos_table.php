c75ee8c2b076f540fd2d83f2415617ae4c0e36ba
salvesvence <salvesvence@gmail.com>
2016-11-17 23:08:26 +0100

    Created the Photo model class.

---
+++ database/migrations/2016_11_17_220732_create_photos_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('post_id')->unsigned();
+            $table->string('sm_thumbnail');
+            $table->string('md_thumbnail');
+            $table->string('lg_thumbnail');
+
+            $table->foreign('post_id')
+                  ->references('id')
+                  ->on('posts')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
