ccabcb6c22ef5caabb8437bf0055d697c0d6ee2d
salvesvence <salvesvence@gmail.com>
2017-01-07 20:16:53 +0100

    Changed the gulp config and added the kartik dependency to the package.json file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const elixir = require('laravel-elixir');
+const elixir = require('laravel-elixir'),
+    inProduction = elixir.config.production;
 
 require('laravel-elixir-vue-2');
+
+elixir.config.sourcemaps = false;
 
 /*
  |--------------------------------------------------------------------------
@@ -14,10 +17,42 @@ require('laravel-elixir-vue-2');
  */
 
 elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js')
-       .version([
-           'css/app.css',
-           'js/app.js'
-       ]);
+    const bootstrapPath = 'node_modules/bootstrap-sass/assets',
+          slickPath = 'node_modules/slick-carousel/slick';
+
+    if (! inProduction) {
+        mix.copy(bootstrapPath + '/fonts', 'public/fonts')
+            .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'resources/assets/js/bootstrap')
+            .copy('node_modules/jquery/dist/jquery.min.js', 'resources/assets/js/jquery')
+            .copy('node_modules/bootstrap-fileinput-npm/js/fileinput.min.js', 'resources/assets/js/kartik')
+            .copy([
+                'node_modules/vue/dist/vue.min.js',
+                'node_modules/vue-resource/dist/vue-resource.min.js'
+            ], 'resources/assets/js/vue')
+            .copy(slickPath + '/fonts', 'public/fonts/slick')
+            .copy(slickPath + '/*.css', 'public/css/slick')
+            .copy(slickPath + '/*.js', 'public/js/slick')
+            .copy(slickPath + '/ajax-loader.gif', 'public/gifs/ajax-loader.gif')
+    }
+
+    mix.sass('app.scss', 'resources/assets/css/app.css')
+
+        .styles(['app.css', 'animate.css'], 'public/css/app.min.css')
+
+        .scripts([
+            'jquery/jquery.min.js',
+            'vue/vue.min.js',
+            'vue/vue-resource.min.js',
+            'kartik/fileinput.min.js',
+            'bootstrap/bootstrap.min.js',
+            'bootstrap/bootstrap.min.js',
+            'scripts/modals.js',
+            'scripts/forms.js'
+        ], 'public/js/app.min.js')
+
+        .webpack([
+            'scripts/list-table.js'
+        ], 'public/js/scripts.js')
+
+        .version(['css/app.min.css', 'js/app.min.js']);
 });
--- resources/views/web/templates/app.blade.php
+++ resources/views/web/templates/app.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
-        <link href="{{ elixir('css/app.min.css') }}" rel="stylesheet">
+        <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 
         @if(Auth::check())
             <script src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
@@ -29,7 +29,7 @@
 
         @yield('content')
 
-        <script src="{{ elixir('js/app.min.js') }}"></script>
+        <script src="{{ elixir('js/app.js') }}"></script>
         <script src="{{ asset('js/scripts.js') }}"></script>
         @yield('kartik-script')
         @yield('slick-script')
