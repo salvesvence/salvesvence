2c27b6c9a99a9fab09cf2fda843e9b3f3ff2220a
salvesvence <salvesvence@gmail.com>
2017-01-07 19:53:02 +0100

    Created the list-table vue script.Q

---
+++ resources/assets/js/scripts/lis-table.js
@@ -0,0 +1,31 @@
+Vue.transition('fade-out', {
+    leaveClass: 'fadeOut'
+});
+
+Vue.component('list-table', {
+    template: '#list-table',
+
+    props: ['list'],
+
+    methods: {
+        delete: function(slug) {
+
+            var elem = $('a#' + slug + '-delete'),
+                url = elem.data('url');
+
+            elem.parents('tr').fadeToggle("slow", "linear");
+
+            this.$http.get('/' + url).then((response) => {
+                console.log(response.data.message);
+            });
+        }
+    },
+
+    created() {
+        this.list = JSON.parse(this.list).data;
+    }
+});
+
+new Vue({
+    el: 'body'
+});
